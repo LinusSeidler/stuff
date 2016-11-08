568e295fac67fb586e6cebd3da963e350bccf2c9
Linus <linus.seidler@experimental-game.com>
2016-11-08 14:55:05 +0100

    finished itsgonnarain, setup musicforairports

--- itsgonnarain/itsgonnarain.js
+++ itsgonnarain/itsgonnarain.js
@@ -1,16 +1,25 @@
 var song = 'MODERAT.mp3';
 
+var _loopStart = 0;//2.95;
+var _loopEnd = 17;//4.05;
 
 
 let audioContext = new AudioContext();
 
-function startLoop(audioBuffer) {
+function startLoop(audioBuffer, pan = 0, rate = 1) {
   let sourceNode = audioContext.createBufferSource();
+  let pannerNode = audioContext.createStereoPanner();
+
   sourceNode.buffer = audioBuffer;
   sourceNode.loop = true;
-  sourceNode.loopStart = 2.95;
-  sourceNode.loopEnd = 4.05;
-  sourceNode.connect(audioContext.destination);
+  sourceNode.loopStart = _loopStart;
+  sourceNode.loopEnd = _loopEnd;
+  sourceNode.playbackRate.value = rate;
+  pannerNode.pan.value = pan;
+  
+  sourceNode.connect(pannerNode);
+  pannerNode.connect(audioContext.destination);
+  
   sourceNode.start(0, sourceNode.loopStart);
 }
 
@@ -18,6 +27,8 @@ fetch(song)
   .then(response => response.arrayBuffer())
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
   .then(audioBuffer => {
-    startLoop(audioBuffer);
+    //startLoop(audioBuffer, 0, 1);
+    //startLoop(audioBuffer, 0, 1.0);
+    startLoop(audioBuffer, 0, 1);
   })
   .catch(e => console.error(e));
